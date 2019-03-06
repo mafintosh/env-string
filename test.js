73e65b2875ad0ac0ad830894aa611225e11484c9
Renée Kooi <renee@kooi.me>
2019-03-06 15:00:20 +0100

    do not replace backslash-escaped variables

--- test.js
+++ test.js
@@ -19,3 +19,20 @@ tape('inlines env array', function (t) {
   t.same(env('hello $FOO $BAR $BAZ', maps), 'hello foo bar ')
   t.end()
 })
+
+tape('does not inline escaped', function (t) {
+  var map = {WORLD: 'world'}
+
+  t.same(env('hello \\$WORLD', map), 'hello $WORLD')
+  // JS/shell sees two backslashes (\\)
+  t.same(env('hello \\\\$WORLD', map), 'hello \\\\world')
+  t.same(env('hello \\${WORLD}', map), 'hello ${WORLD}')
+  t.same(env('hello \\$WORLD-world', map), 'hello $WORLD-world')
+  t.same(env('hello \\${WORLD:-verden}', map), 'hello ${WORLD:-verden}')
+  t.same(env('hello \\${VERDEN:-world}', map), 'hello ${VERDEN:-world}')
+  t.same(env('hello \\$VERDEN', map), 'hello $VERDEN')
+  // JS/shell sees 3 backslashes (\\\), 2 should remain (\\)
+  t.same(env('hello \\\\\\$VERDEN', map), 'hello \\\\$VERDEN')
+  t.same(env('hello \\\\\\\\\\\\\\$VERDEN', map), 'hello \\\\\\\\\\\\$VERDEN')
+  t.end()
+})
