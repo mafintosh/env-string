7f42ccdae89c238da4992a6b0d5be040085ac1bf
Renée Kooi <renee@kooi.me>
2019-03-06 15:00:20 +0100

    do not replace backslash-escaped variables

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 module.exports = replace
 
 function replace (str, env) {
-  return str.replace(/\$({)?([a-z0-9_]+)(:-[^}]+)?(})?/gi, replacer(env || {}))
+  return str.replace(/(\\*)\$({)?([a-z0-9_]+)(:-[^}]+)?(})?/gi, replacer(env || {}))
 }
 
 function replacer (env) {
   return Array.isArray(env) ? replaceArray : replaceEnv
 
-  function replaceArray (_, start, key, def, end) {
+  function replaceArray (input, escapes, start, key, def, end) {
+    if (isEscaped(escapes)) {
+      // Only cut off 1 backslash so the shell that the output will probably be sent to can resolve the remaining ones
+      // (this way users don't have to triple-escape their stuff)
+      return input.slice(1)
+    }
+
     for (var i = 0; i < env.length; i++) {
       if (env[i][key]) return env[i][key]
     }
-    return defaultValue(start, def, end)
+    return escapes + defaultValue(start, def, end)
   }
 
-  function replaceEnv (_, start, key, def, end) {
-    return env[key] || defaultValue(start, def, end)
+  function replaceEnv (input, escapes, start, key, def, end) {
+    if (isEscaped(escapes)) {
+      return input.slice(1)
+    }
+
+    return escapes + (env[key] || defaultValue(start, def, end))
   }
 
   function defaultValue (start, def, end) {
     return (start && def && end) ? replace(def.slice(2), env) : ''
   }
+}
+
+function isEscaped (escapes) {
+  return escapes.length % 2 === 1
 }
